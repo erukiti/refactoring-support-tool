c88864ad3a8685f0b3f6967d7f858d238af9471a
SASAKI Shunsuke <erukiti@gmail.com>
2020-11-17 18:14:17 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,32 @@
-import { transformAsync, ParserOptions } from '@babel/core'
+import {
+  transformAsync,
+  ParserOptions,
+  Visitor,
+  traverse,
+} from '@babel/core'
 
 const parseSource = async (code: string) => {
   const plugins: ParserOptions['plugins'] = ['typescript', 'jsx']
 
+  const visitor: Visitor = {
+    enter(nodePath) {
+      console.log('enter', nodePath.type)
+    },
+    exit(nodePath) {
+      console.log('leave', nodePath.type)
+    },
+  }
+
   const ast = (
-    await transformAsync(code, { ast: true, parserOpts: { plugins } })
+    await transformAsync(code, {
+      parserOpts: { plugins },
+      ast: true,
+    })
   )?.ast
-  return ast
+
+  if (ast) {
+    traverse(ast, visitor)
+  }
 }
 
 parseSource('const a = 1 + 2; console.log(a)').then((ast) =>
