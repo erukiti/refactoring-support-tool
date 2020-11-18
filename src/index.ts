dfc931072ce1837404277a1b06b77ee7c2e711fb
SASAKI Shunsuke <erukiti@gmail.com>
2020-11-18 15:49:59 +0900

    ひとまず第一歩

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,52 @@
-import {
-  transformAsync,
-  ParserOptions,
-  Visitor,
-  traverse,
-} from '@babel/core'
+import { transformAsync, ParserOptions, Node } from '@babel/core'
+// import {File} from '@babel/types'
+
+type Traversed = {
+  code: string | undefined
+  ast: Node
+}
+
+const traverse = (ast: Node): Traversed => {
+  switch (ast.type) {
+    case 'Program': {
+      const code = ast.body.map((node) => traverse(node).code).join('\n')
+      return { code, ast }
+    }
+    case 'ExpressionStatement': {
+      // 一端素通しする？
+      return traverse(ast.expression)
+    }
+    case 'CallExpression': {
+      if (ast.callee.type === 'Identifier') {
+        // いったん、Identifier なら全部未知の物として扱う
+        const args = ast.arguments
+          .map((arg) => traverse(arg).code)
+          .join(', ')
+        console.log(args)
+        return {
+          code: `${ast.callee.name}(${args})`,
+          ast,
+        }
+      } else {
+        break
+      }
+    }
+    case 'NumericLiteral': {
+      return {
+        code: `${ast.value}`,
+        ast,
+      }
+    }
+  }
+  console.log(`UNKNOWN ${ast.type}`, ast)
+  return {
+    code: 'UNKNOWN\n',
+    ast,
+  }
+}
 
 const parseSource = async (code: string) => {
   const plugins: ParserOptions['plugins'] = ['typescript', 'jsx']
-
-  const visitor: Visitor = {
-    enter(nodePath) {
-      console.log('enter', nodePath.type)
-    },
-    exit(nodePath) {
-      console.log('leave', nodePath.type)
-    },
-  }
 
   const ast = (
     await transformAsync(code, {
@@ -24,11 +55,13 @@ const parseSource = async (code: string) => {
     })
   )?.ast
 
-  if (ast) {
-    traverse(ast, visitor)
+  if (!ast) {
+    throw new Error('parsing ast failed.')
+  } else {
+    const analyzed = traverse(ast.program)
+    console.log(analyzed.code)
   }
 }
 
-parseSource('const a = 1 + 2; console.log(a)').then((ast) =>
-  console.log(JSON.stringify(ast, null, '  ')),
-)
+parseSource('alert(3)')
+// parseSource('const a = 1 + 2; console.log(a)')
